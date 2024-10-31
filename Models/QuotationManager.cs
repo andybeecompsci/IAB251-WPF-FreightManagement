9124618acd713274ab3f27884a71d99ae77aa160
Sully <sullybustermove@gmail.com>
2024-10-31 14:58:04 +1000

    user story i3 start

--- Models/QuotationManager.cs
+++ Models/QuotationManager.cs
@@ -26,8 +26,17 @@ namespace IAB251_ASS2.Models
 
 
 
+        // ABOVE STUFF NEEDS TO BE COMPATIBLE WITH THE BELOW
+
         // store quotations in a list
         private List<Quotation> quotations = new List<Quotation>();
+        
+        //make list of pending quotations 
+        public List<Quotation> GetPendingRequests()
+        {
+            return quotations.Where(r => r.Status == "Pending").ToList();
+        }
+
 
         // retreive quotations
         public List<Quotation> GetQuotations()
@@ -39,6 +48,29 @@ namespace IAB251_ASS2.Models
         public Quotation QuotationByNumber(int quotationNumber)
         {
             return quotations.FirstOrDefault(q => q.QuotationNumber == quotationNumber);
+        }
+
+        // accepts quotation and updates status to Accepted
+        public void AcceptQuotationRequest(int requestNumber)
+        {
+            var request = quotations.FirstOrDefault(r => r.QuotationNumber == requestNumber);
+            if (request != null)
+            {
+                request.Status = "Accepted";
+            }
+        }
+
+        // rejects quotation and updates status to Rejected
+        public void RejectQuotationRequest(int requestNumber, string message)
+        {
+            var request = quotations.FirstOrDefault(r => r.QuotationNumber == requestNumber);
+            if (request != null)
+            {
+                request.Status = "Rejected";
+                
+                // add something to send message to customer when they login next
+                //message;
+            }
         }
     }
 }
---
+++ OfficerRequestView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using IAB251_ASS2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IAB251_WPF_ASS2
+{
+    /// <summary>
+    /// Interaction logic for OfficerRequestView.xaml
+    /// </summary>
+    public partial class OfficerRequestView : Window
+    {
+        
+        private QuotationManager quotationManager;
+        public OfficerRequestView()
+        {
+            InitializeComponent();
+            quotationManager = new QuotationManager();
+
+
+        }
+    }
+}
