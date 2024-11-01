a2a1eb0f3cf65f6035295850274553c3b0f8ae48
Anderson Bee <n10791051@qut.edu.au>
2024-11-01 11:19:43 +1000

    redid attributes of QuotationRequest

--- Models/QuotationRequest.cs
+++ Models/QuotationRequest.cs
@@ -28,30 +28,62 @@ namespace IAB251_ASS2.Models
         //public List<string> NatureJob { get; set; } = new List<string>(); //holding import/export, packing/unpacking, quarantine info NEED TO BE DONE
 
         // COMMENTED OUT ABOVE TO MAKE IT MORE COMPATIBLE - TALK TO SULLY ABOUT CHANGES
+        
+        //quotation request
 
+        //info
         public int RequestID { get; set; } 
         public Customer CustomerInfo { get; set; } 
         public string Source { get; set; }
         public string Destination { get; set; }
         public int ContainerQuantity { get; set; }
-        public string NaturePackage { get; set; } // split into Goods type, Width, Height
-        public List<string> NatureJob { get; set; } = new List<string>(); //holding import/export, packing/unpacking, quarantine info NEED TO BE DONE
+
+        //Nature of Package
+        public string GoodsType {  get; set; }
+        public string Width { get; set; }
+        public string Height { get; set; }
+
+        // Nature of Job
         // split into Import/Export, Packing/Unpacking, Quarantine Details, Fumigation Details
+        public string PortType { get; set; }
+        public string PackingType { get; set; }
+        public string QuarantineDetails { get; set; }
+        public string FumigationDetails { get; set; }
+
+        //status
         public string Status { get; set; } = "Pending";
+
+
+        //message
         public string Message { get; set; } = string.Empty;
 
 
         public QuotationRequest() { } //paramaterless constructor to avoid errorrs
 
         //request contents
-        public QuotationRequest(int requestID, Customer customerInfo, string source, string destination, int containerQuantity, List<string> natureJob)
+        public QuotationRequest(int requestID, Customer customerInfo, string source, string destination, int containerQuantity,
+                                string goodsType, string width, string height, string portType, string packingType, string quarantineDetails,
+                                string fumigationDetails, string status)
         {
             RequestID = requestID;
             CustomerInfo = customerInfo;
             Source = source;
             Destination = destination;
             ContainerQuantity = containerQuantity;
-            NatureJob = natureJob;
+
+            //nature of package
+            GoodsType = goodsType;
+            Width = width;
+            Height = height;
+
+            //nature of job
+            PortType = portType;
+            PackingType = packingType;
+            QuarantineDetails = quarantineDetails;
+            FumigationDetails = fumigationDetails;
+
+            //status
+            Status = status;
         }
 
         // set status of customer quotation and any rejection message
